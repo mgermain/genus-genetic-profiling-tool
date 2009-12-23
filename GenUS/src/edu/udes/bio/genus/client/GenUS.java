0b4e60dbad59c631f8dbe19ef2cd2bb348a2c595
Gabriel Girard <girard.gabriel@gmail.com>
2009-12-23 02:29:18 +0000

    javadoc

--- GenUS/src/edu/udes/bio/genus/client/GenUS.java
+++ GenUS/src/edu/udes/bio/genus/client/GenUS.java
@@ -1,3 +1,22 @@
+/*
+ * GenUS: Genetic Profiling Tool v.1.0
+ * Copyright (C) 2009 Université de Sherbrooke
+ * Contact: code.google.com/p/genus-genetic-profiling-tool/
+ * 
+ * This is a free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or any later version.
+ * 
+ * This project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY. See the GNU
+ * Lesser General Public License for more details.
+ *  
+ * Constributors: Mathieu Germain, Gabriel Girard, Alex Rouillard, Alexei Nordell-Markovits
+ * 
+ * December 2009
+ * 
+ */
 package edu.udes.bio.genus.client;
 
 import com.google.gwt.core.client.EntryPoint;
@@ -16,11 +35,16 @@ import edu.udes.bio.genus.client.ui.menu.Prop_Strands;
  */
 public class GenUS extends AbsolutePanel implements EntryPoint {
 
-    // Creating the pool of strands to draw
+    /** The pool of strands to draw */
     public static Pool rnaPool = new Pool();
 
+    /** The display area. */
     public static Drawer displayArea = new Drawer(GenUS.rnaPool);
+
+    /** The main menu. */
     public static Menu mainMenu = new Menu();
+
+    /** The properties menu. */
     public static Prop_Strands propMenu = new Prop_Strands();
 
     /**
