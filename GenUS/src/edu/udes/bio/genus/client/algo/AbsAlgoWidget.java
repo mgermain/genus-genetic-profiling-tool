d0b4c7a0eeea9eb6dd6184593175d839baac2cdc
girard.gabriel <girard.gabriel@f33d04b2-ed2c-11de-91ba-91c70a792394>
2009-12-23 04:41:33 +0000

    javadoc

--- GenUS/src/edu/udes/bio/genus/client/algo/AbsAlgoWidget.java
+++ GenUS/src/edu/udes/bio/genus/client/algo/AbsAlgoWidget.java
@@ -1,14 +1,50 @@
+/*
+ * GenUS: Genetic Profiling Tool v.1.0
+ * Copyright (C) 2009 Université de Sherbrooke
+ * Contact: code.google.com/p/genus-genetic-profiling-tool/
+ * 
+ * This is a free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or any later version.
+ * 
+ * This project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY. See the GNU
+ * Lesser General Public License for more details.
+ *  
+ * Contributors: Mathieu Germain, Gabriel Girard, Alex Rouillard, Alexei Nordell-Markovits
+ * 
+ * December 2009
+ * 
+ */
 package edu.udes.bio.genus.client.algo;
 
 import com.google.gwt.user.client.ui.Composite;
 
+/**
+ * The Class AbsAlgoWidget.
+ */
 public abstract class AbsAlgoWidget extends Composite {
     protected final static String EMPTYSTRING = "";
 
+    /**
+     * Gets the name.
+     * 
+     * @return the name
+     */
     public abstract String getName();
 
+    /**
+     * Gets the description.
+     * 
+     * @return the description
+     */
     public abstract String getDescription();
 
+    /**
+     * Show options.
+     * display the algorithm dialog.
+     */
     public abstract void showOptions();
 
 }
--- GenUS/src/edu/udes/bio/genus/client/algo/WrapperRNA.java
+++ GenUS/src/edu/udes/bio/genus/client/algo/WrapperRNA.java
@@ -1,14 +1,41 @@
+/*
+ * GenUS: Genetic Profiling Tool v.1.0
+ * Copyright (C) 2009 Université de Sherbrooke
+ * Contact: code.google.com/p/genus-genetic-profiling-tool/
+ * 
+ * This is a free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or any later version.
+ * 
+ * This project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY. See the GNU
+ * Lesser General Public License for more details.
+ *  
+ * Contributors: Mathieu Germain, Gabriel Girard, Alex Rouillard, Alexei Nordell-Markovits
+ * 
+ * December 2009
+ * 
+ */
 package edu.udes.bio.genus.client.algo;
 
 import java.io.Serializable;
 
 import edu.udes.bio.genus.client.rna.RNAss;
 
+/**
+ * The Class WrapperRNA.
+ * ****should be generalize
+ */
 public class WrapperRNA implements Serializable {
     /**
      * 
      */
     private static final long serialVersionUID = 1L;
+
+    /** The rna secondary structure. */
     public RNAss rna;
+
+    /** The match. */
     public double match;
 }
